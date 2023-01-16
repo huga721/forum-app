67cc3e9ac4233f1f9ea125d990d35586d4b1c9b8
hubert <szabahubert@gmail.com>
2023-01-16 21:18:50 +0100

    added post repository

---
+++ src/main/java/huberts/spring/forumapp/post/PostRepository.java
@@ -0,0 +1,8 @@
+package huberts.spring.forumapp.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
