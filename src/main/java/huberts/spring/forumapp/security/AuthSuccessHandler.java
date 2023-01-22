758bcaf641bd6b4dbc9b98f6a9539eeb0a4d515f
hubert <szabahubert@gmail.com>
2023-01-22 19:02:11 +0100

    small changes in success handler

--- src/main/java/huberts/spring/forumapp/security/AuthSuccessHandler.java
+++ src/main/java/huberts/spring/forumapp/security/AuthSuccessHandler.java
@@ -3,7 +3,6 @@ package huberts.spring.forumapp.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
