63a566a592cb33982b49926f653f65fd9549401e
hubert <szabahubert@gmail.com>
2023-02-01 20:28:24 +0100

    added custom annotations for security and changed security config

---
+++ src/main/java/huberts/spring/forumapp/annotation/AdminRole.java
@@ -0,0 +1,14 @@
+package huberts.spring.forumapp.annotation;
+
+import org.springframework.security.access.annotation.Secured;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Secured("ROLE_ADMIN")
+public @interface AdminRole {
+}
--- src/main/java/huberts/spring/forumapp/security/SecurityConfig.java
+++ src/main/java/huberts/spring/forumapp/security/SecurityConfig.java
@@ -4,9 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
@@ -14,8 +14,8 @@ import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 
-// Class that configures Security, here you put the rules of your security, authorize endpoints, add filters
 @EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
@@ -33,13 +33,6 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> {
-                auth
-                        .requestMatchers("/register").permitAll()
-                        .requestMatchers("/swagger-ui/**").permitAll()
-                        .requestMatchers("/v3/**").permitAll()
-                        .anyRequest().authenticated();
-                })
                 .exceptionHandling()
                 .authenticationEntryPoint(new HttpStatusEntryPoint(UNAUTHORIZED))
                 .and()
