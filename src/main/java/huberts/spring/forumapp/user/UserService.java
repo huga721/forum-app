4b755474bc50120d651257e9409b185ea6813e50
huga721 <szabahubert@gmail.com>
2023-02-19 20:42:35 +0000

    created user service methods

--- src/main/java/huberts/spring/forumapp/user/UserService.java
+++ src/main/java/huberts/spring/forumapp/user/UserService.java
@@ -1,73 +1,123 @@
 package huberts.spring.forumapp.user;
 
+import huberts.spring.forumapp.exception.RoleDoesntExistException;
 import huberts.spring.forumapp.exception.UserAlreadyExistingException;
+import huberts.spring.forumapp.exception.UserDoesntExistException;
+import huberts.spring.forumapp.exception.UsernameOrPasswordIsBlankOrEmpty;
 import huberts.spring.forumapp.role.Role;
 import huberts.spring.forumapp.role.RoleRepository;
 import huberts.spring.forumapp.user.dto.RegisterDTO;
 import huberts.spring.forumapp.user.dto.UserDTO;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.stream.Collectors;
 
+@Transactional
 @Service
-@Slf4j
 @RequiredArgsConstructor
-public class UserService implements UserServiceApi{
+public class UserService implements UserServiceApi {
+
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final UserMapper mapper;
 
     @Override
     public UserDTO addUser(RegisterDTO user) {
         String username = user.getUsername();
+        String password = passwordEncoder.encode(user.getPassword());
+        Role roleUser = roleRepository.findByName("ROLE_USER");
+
         if (userRepository.existsByUsername(username)) {
             throw new UserAlreadyExistingException("User " + username + " already existing in database.");
         }
-        String password = user.getPassword();
-        Role roleUser = roleRepository.findByName("ROLE_USER");
+        if (username.isBlank() || user.getPassword().isBlank()) {
+            throw new UsernameOrPasswordIsBlankOrEmpty("Username or password is blank or empty.");
+        }
 
-        User userCreated = userBuilder(username, password, roleUser);
+        User userCreated = mapper.buildUser(username, password, roleUser);
         userRepository.save(userCreated);
 
-        UserDTO userDTO = userDTOBuilder(username);
-        return userDTO;
+        return mapper.buildUserDTO(userCreated);
     }
 
     @Override
-    public Optional<User> findUser(String name) {
-        return userRepository.findByUsername(name);
+    public List<UserDTO> findAllUsers() {
+        return mapper.mapFromList(userRepository.findAll());
     }
 
     @Override
-    public List<User> findAll() {
-        return userRepository.findAll();
+    public List<UserDTO> findAllStaffUsers() {
+        return mapper.mapFromList(userRepository.findAll())
+                .stream()
+                .filter(user ->
+                        user.getRole().equals("ROLE_ADMIN") ||
+                                user.getRole().equals("ROLE_MODERATOR"))
+                .collect(Collectors.toList());
     }
 
     @Override
-    public void deleteUser(String username) {
-        var user = findUser(username).get();
+    public User findUser(String username) {
+        if (!userRepository.existsByUsername(username)) {
+            throw new UserDoesntExistException("User " + username + " doesn't exist.");
+        }
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public void deleteUserByUsername(String username) {
+        User user = findUser(username);
         userRepository.delete(user);
     }
 
-    private static UserDTO userDTOBuilder(String username) {
-        return UserDTO.builder()
-                .username(username)
-                .build();
+    @Override
+    public UserDTO currentUser(String username) {
+        User user = userRepository.findByUsername(username);
+        return mapper.buildUserDTO(user);
     }
 
-    private static User userBuilder(String username, String password, Role role) {
-        Set<Role> roles = new HashSet<>();
-        roles.add(role);
-        return User.builder()
-                .username(username)
-                .password(password)
-                .roles(roles)
-                .build();
+    @Override
+    public User currentLoggedUser(String token) {
+        return null;
     }
 
+    @Override
+    public UserDTO changeRole(String username, String roleName) {
+        if (!userRepository.existsByUsername(username)) {
+            throw new UserDoesntExistException("User " + username + " doesn't exist.");
+        }
+        if (!roleRepository.existsByName(roleName)) {
+            throw new RoleDoesntExistException("Role " + roleName + "doesn't exist.");
+        }
+
+        User user = userRepository.findByUsername(username);
+        Role role = roleRepository.findByName(roleName);
+
+        user.setRole(role);
+        return mapper.buildUserDTO(user);
+    }
+
+    @Override
+    public UserDTO banUser(String username) {
+        User user = findUser(username);
+        user.setBlocked(true);
+        return mapper.buildUserDTO(user);
+    }
+
+    @Override
+    public UserDTO unbanUser(String username) {
+        User user = findUser(username);
+        user.setBlocked(false);
+        return mapper.buildUserDTO(user);
+    }
+
+    @Override
+    public UserDTO findUserDTO(String username) {
+        User user = findUser(username);
+        return mapper.buildUserDTO(user);
+    }
 }
