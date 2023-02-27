6b313ee757015ea8dcb3053da00e0584dfbbb649
hubert <szabahubert@gmail.com>
2023-02-27 22:27:07 +0100

    implemented new methods for category controller

--- src/main/java/huberts/spring/forumapp/category/CategoryController.java
+++ src/main/java/huberts/spring/forumapp/category/CategoryController.java
@@ -1,7 +1,11 @@
 package huberts.spring.forumapp.category;
 
 import huberts.spring.forumapp.category.dto.CategoryDTO;
-import huberts.spring.forumapp.category.dto.CreateDTO;
+import huberts.spring.forumapp.category.dto.CategoryCreateDTO;
+import huberts.spring.forumapp.category.dto.CategoryDescriptionDTO;
+import huberts.spring.forumapp.category.dto.CategoryTitleDTO;
+import huberts.spring.forumapp.security.annotation.AdminRole;
+import huberts.spring.forumapp.security.annotation.ModeratorRole;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,17 +19,43 @@ import java.util.List;
 public class CategoryController {
     private final CategoryService service;
 
-    @PostMapping("/create")
-    ResponseEntity<CategoryDTO> create (@RequestBody CreateDTO category) {
-        CategoryDTO categoryCreated = service.createCategory(category);
-        return ResponseEntity.created(URI.create("/categories")).body(categoryCreated);
+    @GetMapping("/all")
+    ResponseEntity<List<CategoryDTO>> getAll() {
+        List<CategoryDTO> categories = service.findAll();
+        return ResponseEntity.ok(categories);
     }
 
-    @DeleteMapping("/delete/")
-    ResponseEntity
+    @GetMapping("/{title}")
+    ResponseEntity<CategoryDTO> getByTitle(@PathVariable String title) {
+        CategoryDTO category = service.findCategoryDTO(title);
+        return ResponseEntity.ok(category);
+    }
 
-    @GetMapping("/getAllCategories")
-    ResponseEntity<List<Category>> getAll () {
-        return ResponseEntity.ok(service.findAll());
+    @ModeratorRole
+    @PostMapping("/moderator/create")
+    ResponseEntity<CategoryDTO> create(@RequestBody CategoryCreateDTO create) {
+        CategoryDTO category = service.saveNewCategory(create);
+        return ResponseEntity.created(URI.create("/categories")).body(category);
+    }
+
+    @AdminRole
+    @PatchMapping("/admin/{title}/title")
+    ResponseEntity<CategoryDTO> changeTitle(@PathVariable String title, @RequestBody CategoryTitleDTO newTitle) {
+        CategoryDTO category = service.changeTitle(title, newTitle);
+        return ResponseEntity.ok(category);
+    }
+
+    @AdminRole
+    @PatchMapping("/admin/{title}/description")
+    ResponseEntity<CategoryDTO> changeDescription(@PathVariable String title, @RequestBody CategoryDescriptionDTO description) {
+        CategoryDTO category = service.changeDescription(title, description);
+        return ResponseEntity.ok(category);
+    }
+
+    @AdminRole
+    @DeleteMapping("/admin/{title}/delete")
+    ResponseEntity<Void> deleteCategory(@PathVariable String title) {
+        service.deleteCategory(title);
+        return ResponseEntity.noContent().build();
     }
 }
