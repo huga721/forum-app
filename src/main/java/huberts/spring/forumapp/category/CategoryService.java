ea2acdcda32c1e9d6c2aaf486b11e415e32b193b
hubert <szabahubert@gmail.com>
2023-02-27 22:25:59 +0100

    implemented new methods for category service

--- src/main/java/huberts/spring/forumapp/category/CategoryService.java
+++ src/main/java/huberts/spring/forumapp/category/CategoryService.java
@@ -1,48 +1,116 @@
 package huberts.spring.forumapp.category;
 
 import huberts.spring.forumapp.category.dto.CategoryDTO;
-import huberts.spring.forumapp.category.dto.CreateDTO;
+import huberts.spring.forumapp.category.dto.CategoryCreateDTO;
+import huberts.spring.forumapp.category.dto.CategoryDescriptionDTO;
+import huberts.spring.forumapp.category.dto.CategoryTitleDTO;
 import huberts.spring.forumapp.exception.CategoryAlreadyExistingException;
+import huberts.spring.forumapp.exception.CategoryDescriptionException;
+import huberts.spring.forumapp.exception.CategoryDoesntExistException;
+import huberts.spring.forumapp.exception.CategoryTitleException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class CategoryService implements CategoryServiceApi {
+
     private final CategoryRepository repository;
 
     @Override
-    public CategoryDTO createCategory(CreateDTO category) {
-        String title = category.getTitle();
-        if (repository.existsByTitle(title)) {
-            throw new CategoryAlreadyExistingException("Category " + category.getTitle() +
+    public CategoryDTO saveNewCategory(CategoryCreateDTO category) {
+        String categoryTitle = category.getTitle();
+        String categoryDescription = category.getDescription();
+
+        if (categoryTitle.isBlank()) {
+            throw new CategoryTitleException("Title is blank or empty.");
+        }
+        if (categoryDescription.isBlank()) {
+            throw new CategoryDescriptionException("Description is blank or empty.");
+        }
+        if (repository.existsByTitle(categoryTitle)) {
+            throw new CategoryAlreadyExistingException("Category " + categoryTitle +
                     " already exists, please choose another title.");
         }
+        if (categoryTitle.endsWith(" ")) {
+            throw new CategoryTitleException("Title can't end with white space.");
+        }
+        if (categoryDescription.endsWith(" ")) {
+            throw new CategoryDescriptionException("Description can't end with white space.");
+        }
 
-        Category categoryCreated = categoryBuilder(title);
-        repository.save(categoryCreated);
+        Category created = CategoryMapper.buildNewCategory(category);
+        Category result = repository.save(created);
 
-        return categoryDTOBuilder(categoryCreated);
+        return CategoryMapper.buildCategoryDTO(result);
     }
 
     @Override
-    public List<Category> findAll() {
-        return repository.findAll();
+    public List<CategoryDTO> findAll() {
+        return CategoryMapper.mapFromListCategory(repository.findAll());
     }
 
-    private static Category categoryBuilder (String title) {
-        return Category.builder()
-                .title(title)
-                .posts(null)
-                .build();
+    @Override
+    public CategoryDTO findCategoryDTO(String categoryTitle) {
+        if (!repository.existsByTitle(categoryTitle)) {
+            throw new CategoryDoesntExistException("Category " + categoryTitle + " doesnt exist.");
+        }
+        Category result = repository.findByTitle(categoryTitle);
+        return CategoryMapper.buildCategoryDTO(result);
     }
-    private static CategoryDTO categoryDTOBuilder (Category category) {
-        return CategoryDTO.builder()
-                .id(category.getId())
-                .title(category.getTitle())
-                .posts(category.getPosts())
-                .build();
+
+    @Override
+    public CategoryDTO changeTitle(String categoryTitle, CategoryTitleDTO newCategoryTitle) {
+        String newTitle = newCategoryTitle.getTitle();
+
+        if (newTitle.isBlank() || categoryTitle.isBlank()) {
+            throw new CategoryTitleException("Title is blank or empty.");
+        }
+        if (categoryTitle.equals(newTitle)) {
+            throw new CategoryTitleException("New title is the same as actual title in category.");
+        }
+
+        Category result = findCategory(categoryTitle);
+        result.setTitle(newTitle);
+
+        return CategoryMapper.buildCategoryDTO(result);
+    }
+
+    @Override
+    public CategoryDTO changeDescription(String categoryTitle, CategoryDescriptionDTO newCategoryDescription) {
+        String newDescription = newCategoryDescription.getDescription();
+
+        if (newDescription.isBlank()) {
+            throw new CategoryDescriptionException("Category description is blank or empty.");
+        }
+
+        Category category = findCategory(categoryTitle);
+        String actualDescription = category.getDescription();
+
+        if (newDescription.equals(actualDescription)) {
+            throw new CategoryDescriptionException("Categories descriptions are the same.");
+        }
+
+        category.setDescription(newDescription);
+
+        return CategoryMapper.buildCategoryDTO(category);
+    }
+
+    @Override
+    public void deleteCategory(String categoryTitle) {
+        Category result = findCategory(categoryTitle);
+        repository.delete(result);
+    }
+
+    @Override
+    public Category findCategory(String categoryTitle) {
+        if (!repository.existsByTitle(categoryTitle)) {
+            throw new CategoryDoesntExistException("Category " + categoryTitle + " doesnt exist.");
+        }
+        return repository.findByTitle(categoryTitle);
     }
 }
