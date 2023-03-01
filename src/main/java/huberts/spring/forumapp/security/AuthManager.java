8ef106850d534f2ec74ff4bd88e90a010b977132
hubert <szabahubert@gmail.com>
2023-03-01 21:57:11 +0100

    code refactor, deleted comments

--- src/main/java/huberts/spring/forumapp/security/AuthManager.java
+++ src/main/java/huberts/spring/forumapp/security/AuthManager.java
@@ -1,5 +1,6 @@
 package huberts.spring.forumapp.security;
 
+import huberts.spring.forumapp.exception.AccountBlockedException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,18 +16,15 @@ import org.springframework.stereotype.Component;
 public class AuthManager implements AuthenticationManager {
     private final DetailsService service;
 
-    /**
-     * Method authenticate user, service is looking for the user in db, if the user exist then creates new token and return it
-     * If it doesn't exist it should throw an exception
-     */
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        // Retrieving a UserDetails object from method which find the user by username in db
         UserDetails user = service.loadUserByUsername(authentication.getPrincipal().toString());
 
-        log.info("AuthManager call \'authenticate\' method");
+        if (!user.isAccountNonLocked()) {
+            throw new AccountBlockedException("User " + user.getUsername() + " is blocked.");
+        }
+
         log.info("username - {} | password - {}", user.getUsername(), user.getPassword());
-        // Returning new UsernamePasswordAuthenticationToken
         return new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword(), user.getAuthorities());
     }
 }
