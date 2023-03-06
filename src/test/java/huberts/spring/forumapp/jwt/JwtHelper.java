4710e5d5acfba7e46472761b98be923b42cc7337
hubert <szabahubert@gmail.com>
2023-03-06 21:42:58 +0100

    Deleted comments

--- src/test/java/huberts/spring/forumapp/jwt/JwtHelper.java
+++ src/test/java/huberts/spring/forumapp/jwt/JwtHelper.java
@@ -8,25 +8,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-/**
- * Class provides a helper methods that can be used to generate a JWT.
- */
 public class JwtHelper {
 
     private static final String LOGIN_ENDPOINT = "/login";
     private static final String BEARER_PREFIX = "Bearer ";
     private static final String TOKEN = "token";
 
-    /**
-     * Generates JWT based on provided user login credentials, using the given ObjectMapper to convert the credentials into
-     * a JSON string, then mocking the JSON string to get the JWT for provided user.
-     *
-     * @param credentials
-     * @param objectMapper
-     * @param mockMvc
-     * @return
-     * @throws Exception
-     */
     static String returnJwt(LoginDTO credentials, ObjectMapper objectMapper, MockMvc mockMvc) throws Exception {
         String jsonCredentials = objectMapper.writeValueAsString(credentials);
 
@@ -38,7 +25,6 @@ public class JwtHelper {
                 .getResponse()
                 .getContentAsString();
 
-        String token = BEARER_PREFIX + objectMapper.readTree(jsonResult).get(TOKEN).asText();
-        return token;
+        return BEARER_PREFIX + objectMapper.readTree(jsonResult).get(TOKEN).asText();
     }
 }
--- src/test/java/huberts/spring/forumapp/jwt/JwtKey.java
+++ src/test/java/huberts/spring/forumapp/jwt/JwtKey.java
@@ -4,9 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import huberts.spring.forumapp.user.dto.LoginDTO;
 import org.springframework.test.web.servlet.MockMvc;
 
-/**
- * A helper class for generating JWT tokens for different types of users.
- */
 public class JwtKey {
 
     private static final String USER_USERNAME = "userJwt";
@@ -15,14 +12,6 @@ public class JwtKey {
     private static final String ADMIN_USERNAME = "adminJwt";
     private static final String PASSWORD = "encrypted_password";
 
-    /**
-     * Generates a JWT token for a user.
-     *
-     * @param mockMvc the MockMvc object to user for the request
-     * @param objectMapper the ObjectMapper object to user for JSON serialization
-     * @return a JWT token
-     * @throws Exception if an error occurs during the request
-     */
     public static String getUserJwt(MockMvc mockMvc, ObjectMapper objectMapper) throws Exception {
         return getJwt(mockMvc, objectMapper, USER_USERNAME);
     }
@@ -31,26 +20,10 @@ public class JwtKey {
         return getJwt(mockMvc, objectMapper, USER_TO_DELETE_USERNAME);
     }
 
-    /**
-     * Generates a JWT token for a moderator.
-     *
-     * @param mockMvc the MockMvc object to use for the request
-     * @param objectMapper the ObjectMapper object to use for JSON serialization
-     * @return a JWT token
-     * @throws Exception if an error occurs during the request
-     */
     public static String getModeratorJwt(MockMvc mockMvc, ObjectMapper objectMapper) throws Exception {
         return getJwt(mockMvc, objectMapper, MODERATOR_USERNAME);
     }
 
-    /**
-     * Generates a JWT token for an admin.
-     *
-     * @param mockMvc the MockMvc object to use for the request
-     * @param objectMapper the ObjectMapper object to use for JSON serialization
-     * @return a JWT token
-     * @throws Exception if an error occurs during the request
-     */
     public static String getAdminJwt(MockMvc mockMvc, ObjectMapper objectMapper) throws Exception {
         return getJwt(mockMvc, objectMapper, ADMIN_USERNAME);
     }
