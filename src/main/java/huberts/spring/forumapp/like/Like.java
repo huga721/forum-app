243e706d83ca3cecd5efcb8850ab4b23020d2e1a
hubert <szabahubert@gmail.com>
2023-03-13 22:35:40 +0100

    added like entity

--- src/main/java/huberts/spring/forumapp/like/Like.java
+++ src/main/java/huberts/spring/forumapp/like/Like.java
@@ -1,19 +1,29 @@
-//package huberts.spring.forumapp.like;
-//
-//import huberts.spring.forumapp.post.Post;
-//import huberts.spring.forumapp.user.User;
-//import jakarta.persistence.*;
-//
-//import java.util.Set;
-//
-//@Entity(name = "`like`")
-//public class Like {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//    @ManyToOne
-//    @JoinColumn(name = "user_id")
-//    private User user;
-////    @OneToMany
-////    private Set<Post> posts;
-//}
+package huberts.spring.forumapp.like;
+
+import huberts.spring.forumapp.comment.Comment;
+import huberts.spring.forumapp.topic.Topic;
+import huberts.spring.forumapp.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "likes")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne()
+    @JoinColumn(name = "topic_id")
+    private Topic topic;
+    @ManyToOne()
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+}
