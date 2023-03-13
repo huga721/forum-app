ab9ecef34f75247d5ca7fd83e1aa46e9fe0057c5
hubert <szabahubert@gmail.com>
2023-03-13 22:31:07 +0100

    added hibernate validation

--- src/main/java/huberts/spring/forumapp/category/dto/CategoryCreateDTO.java
+++ src/main/java/huberts/spring/forumapp/category/dto/CategoryCreateDTO.java
@@ -1,9 +1,13 @@
 package huberts.spring.forumapp.category.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CategoryCreateDTO {
+    @NotBlank(message = "Title is blank.")
     private String title;
+    @Size(max = 50)
     private String description;
 }
--- src/main/java/huberts/spring/forumapp/category/dto/CategoryDescriptionDTO.java
+++ src/main/java/huberts/spring/forumapp/category/dto/CategoryDescriptionDTO.java
@@ -1,8 +1,10 @@
 package huberts.spring.forumapp.category.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class CategoryDescriptionDTO {
+    @NotBlank(message = "Category description is blank.")
     private String description;
 }
