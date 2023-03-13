1d8691ee1c667a19b8c9a933911ac7cfc672ee5f
hubert <szabahubert@gmail.com>
2023-03-13 22:37:18 +0100

    added comment entity class

---
+++ src/main/java/huberts/spring/forumapp/comment/Comment.java
@@ -0,0 +1,34 @@
+package huberts.spring.forumapp.comment;
+
+import huberts.spring.forumapp.like.Like;
+import huberts.spring.forumapp.report.Report;
+import huberts.spring.forumapp.topic.Topic;
+import huberts.spring.forumapp.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity(name = "comment")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+    private LocalDateTime createdAt;
+    @ManyToOne()
+    private Topic topic;
+    @ManyToOne()
+    @JoinColumn(name = "user_id")
+    private User user;
+    @OneToMany(mappedBy = "comment", cascade = CascadeType.REMOVE)
+    private List<Like> likes;
+    @OneToMany(mappedBy = "comment", cascade = CascadeType.REMOVE)
+    private List<Report> reports;
+}
