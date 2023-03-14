9676f9301487c50b94e026b61cc6351f3b498bc2
hubert <szabahubert@gmail.com>
2023-03-14 11:11:11 +0100

    added comment mapper class for object transfers usage

---
+++ src/main/java/huberts/spring/forumapp/comment/CommentMapper.java
@@ -0,0 +1,47 @@
+package huberts.spring.forumapp.comment;
+
+import huberts.spring.forumapp.comment.dto.CommentDTO;
+import huberts.spring.forumapp.like.Like;
+import huberts.spring.forumapp.like.LikeMapper;
+import huberts.spring.forumapp.like.dto.LikeDTO;
+import huberts.spring.forumapp.topic.Topic;
+import huberts.spring.forumapp.user.User;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class CommentMapper {
+
+    public static Comment buildNewComment(User user, Topic topic, String content) {
+        return Comment.builder()
+                .createdAt(LocalDateTime.now())
+                .content(content)
+                .user(user)
+                .topic(topic)
+                .likes(List.of())
+                .build();
+    }
+
+    public static CommentDTO buildCommentDTO(Comment comment) {
+        return CommentDTO.builder()
+                .id(comment.getId())
+                .author(comment.getUser().getUsername())
+                .createdAt(comment.getCreatedAt())
+                .content(comment.getContent())
+                .likes(mapToLikeDTO(comment.getLikes()))
+                .build();
+    }
+
+    public static List<CommentDTO> mapToCommentDTO(List<Comment> comments){
+        return comments.stream()
+                .map(CommentMapper::buildCommentDTO)
+                .collect(Collectors.toList());
+    }
+
+    private static List<LikeDTO> mapToLikeDTO(List<Like> likes) {
+        return LikeMapper.mapLikesToLikeDTO(likes);
+    }
+}
