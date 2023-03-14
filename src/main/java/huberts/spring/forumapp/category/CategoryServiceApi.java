0b672f16f9b0fcef348364664ce2bef904d080f0
hubert <szabahubert@gmail.com>
2023-03-14 14:47:32 +0100

    reorganised methods in category service interface

--- src/main/java/huberts/spring/forumapp/category/CategoryServiceApi.java
+++ src/main/java/huberts/spring/forumapp/category/CategoryServiceApi.java
@@ -8,11 +8,13 @@ import huberts.spring.forumapp.category.dto.CategoryTitleDTO;
 import java.util.List;
 
 public interface CategoryServiceApi {
-    Category findCategory(String title);
-    CategoryDTO saveNewCategory(CategoryCreateDTO category);
-    List<CategoryDTO> findAll();
-    CategoryDTO findCategoryDTO(String title);
-    CategoryDTO changeTitle(String existingCategory, CategoryTitleDTO newCategoryTitle);
-    void deleteCategory(String title);
-    CategoryDTO changeDescription(String title, CategoryDescriptionDTO newDescription);
+    CategoryDTO createCategory(CategoryCreateDTO category);
+
+    CategoryDTO getCategoryById(Long id);
+    List<CategoryDTO> getAllCategories();
+
+    CategoryDTO updateTitle(Long id, CategoryTitleDTO newCategoryTitle);
+    CategoryDTO updateDescription(Long id, CategoryDescriptionDTO newDescription);
+
+    void deleteCategory(Long id);
 }
