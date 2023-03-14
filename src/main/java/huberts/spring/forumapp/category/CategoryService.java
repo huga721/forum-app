1629b84058753e935e8fef22a4cbe297b1173bd8
hubert <szabahubert@gmail.com>
2023-03-14 14:47:05 +0100

    added logs, refactored code and validation in category service

--- src/main/java/huberts/spring/forumapp/category/CategoryService.java
+++ src/main/java/huberts/spring/forumapp/category/CategoryService.java
@@ -4,16 +4,18 @@ import huberts.spring.forumapp.category.dto.CategoryDTO;
 import huberts.spring.forumapp.category.dto.CategoryCreateDTO;
 import huberts.spring.forumapp.category.dto.CategoryDescriptionDTO;
 import huberts.spring.forumapp.category.dto.CategoryTitleDTO;
-import huberts.spring.forumapp.exception.CategoryAlreadyExistingException;
-import huberts.spring.forumapp.exception.CategoryDescriptionException;
-import huberts.spring.forumapp.exception.CategoryDoesntExistException;
-import huberts.spring.forumapp.exception.CategoryTitleException;
+import huberts.spring.forumapp.exception.category.CategoryAlreadyExistingException;
+import huberts.spring.forumapp.exception.category.CategoryDescriptionException;
+import huberts.spring.forumapp.exception.category.CategoryExistException;
+import huberts.spring.forumapp.exception.category.CategoryTitleException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -21,96 +23,99 @@ public class CategoryService implements CategoryServiceApi {
 
     private final CategoryRepository repository;
 
+    private static final String CATEGORY_DOESNT_EXIST_EXCEPTION = "Category with title \"%s\" doesn't exist.";
+    private static final String CATEGORY_ALREADY_EXISTS_EXCEPTION = "Category \"%s\" already exists, please choose another title.";
+    private static final String TITLE_IS_THE_SAME_EXCEPTION = "New title is the same as actual title in category.";
+    private static final String DESCRIPTION_ARE_THE_SAME_EXCEPTION = "New description is the same as actual description in category.";
+    private static final String EXCEPTION_OCCURRED = "An exception occurred!";
+
     @Override
-    public CategoryDTO saveNewCategory(CategoryCreateDTO category) {
-        String categoryTitle = category.getTitle();
-        String categoryDescription = category.getDescription();
+    public CategoryDTO createCategory(CategoryCreateDTO category) {
+        String title = category.getTitle();
+        log.info("Creating category with title {}", title);
 
-        if (categoryTitle.isBlank()) {
-            throw new CategoryTitleException("Title is blank or empty.");
-        }
-        if (categoryDescription.isBlank()) {
-            throw new CategoryDescriptionException("Description is blank or empty.");
-        }
-        if (repository.existsByTitle(categoryTitle)) {
-            throw new CategoryAlreadyExistingException("Category " + categoryTitle +
-                    " already exists, please choose another title.");
-        }
-        if (categoryTitle.endsWith(" ")) {
-            throw new CategoryTitleException("Title can't end with white space.");
-        }
-        if (categoryDescription.endsWith(" ")) {
-            throw new CategoryDescriptionException("Description can't end with white space.");
+        if (repository.existsByTitle(title)) {
+            String errorMessage = String.format(CATEGORY_ALREADY_EXISTS_EXCEPTION, title);
+            log.error(EXCEPTION_OCCURRED, new CategoryAlreadyExistingException(errorMessage));
+            throw new CategoryAlreadyExistingException(String.format(errorMessage));
         }
 
-        Category created = CategoryMapper.buildNewCategory(category);
-        Category result = repository.save(created);
+        Category categoryBuilt = CategoryMapper.buildNewCategory(category);
+        Category categorySaved = repository.save(categoryBuilt);
 
-        return CategoryMapper.buildCategoryDTO(result);
+        log.info("Category created");
+        return CategoryMapper.buildCategoryDTO(categorySaved);
     }
 
     @Override
-    public List<CategoryDTO> findAll() {
+    public CategoryDTO getCategoryById(Long id) {
+        log.info("Getting category with id {}", id);
+        Category categoryFound = findCategory(id);
+        return CategoryMapper.buildCategoryDTO(categoryFound);
+    }
+
+    private Category findCategory(Long id) {
+        log.info("Finding category with id id {}", id);
+        return repository.findById(id)
+                .orElseThrow(() -> {
+                    String errorMessage = String.format(CATEGORY_DOESNT_EXIST_EXCEPTION, id);
+                    log.error(EXCEPTION_OCCURRED, new CategoryExistException(errorMessage));
+                    return new CategoryExistException(errorMessage);
+                });
+    }
+
+    @Override
+    public List<CategoryDTO> getAllCategories() {
+        log.info("Getting all categories");
         return CategoryMapper.mapFromListCategory(repository.findAll());
     }
 
     @Override
-    public CategoryDTO findCategoryDTO(String categoryTitle) {
-        if (!repository.existsByTitle(categoryTitle)) {
-            throw new CategoryDoesntExistException("Category " + categoryTitle + " doesnt exist.");
-        }
-        Category result = repository.findByTitle(categoryTitle);
-        return CategoryMapper.buildCategoryDTO(result);
-    }
+    public CategoryDTO updateTitle(Long id, CategoryTitleDTO newTitleDTO) {
+        log.info("Updating title of category with id {} by moderator or admin", id);
+        String newTitle = newTitleDTO.getCategoryTitle();
 
-    @Override
-    public CategoryDTO changeTitle(String categoryTitle, CategoryTitleDTO newCategoryTitle) {
-        String newTitle = newCategoryTitle.getTitle();
+        Category categoryToUpdate = findCategory(id);
+        String categoryTitle = categoryToUpdate.getTitle();
 
-        if (newTitle.isBlank() || categoryTitle.isBlank()) {
-            throw new CategoryTitleException("Title is blank or empty.");
-        }
         if (categoryTitle.equals(newTitle)) {
-            throw new CategoryTitleException("New title is the same as actual title in category.");
+            log.error(EXCEPTION_OCCURRED, new CategoryTitleException(TITLE_IS_THE_SAME_EXCEPTION));
+            throw new CategoryTitleException(TITLE_IS_THE_SAME_EXCEPTION);
+        }
+        if (repository.existsByTitle(newTitle)) {
+            String errorMessage = String.format(CATEGORY_ALREADY_EXISTS_EXCEPTION, newTitle);
+            log.error(EXCEPTION_OCCURRED, new CategoryTitleException(String.format(errorMessage)));
+            throw new CategoryTitleException(String.format(errorMessage));
         }
 
-        Category result = findCategory(categoryTitle);
-        result.setTitle(newTitle);
-
-        return CategoryMapper.buildCategoryDTO(result);
+        categoryToUpdate.setTitle(newTitle);
+        log.info("Category updated");
+        return CategoryMapper.buildCategoryDTO(categoryToUpdate);
     }
 
     @Override
-    public CategoryDTO changeDescription(String categoryTitle, CategoryDescriptionDTO newCategoryDescription) {
+    public CategoryDTO updateDescription(Long id, CategoryDescriptionDTO newCategoryDescription) {
+        log.info("Updating description of category with id {} by moderator or admin", id);
         String newDescription = newCategoryDescription.getDescription();
 
-        if (newDescription.isBlank()) {
-            throw new CategoryDescriptionException("Category description is blank or empty.");
+        Category categoryToUpdate = findCategory(id);
+        String categoryDescription = categoryToUpdate.getDescription();
+
+        if (newDescription.equals(categoryDescription)) {
+            log.error(EXCEPTION_OCCURRED, new CategoryDescriptionException(DESCRIPTION_ARE_THE_SAME_EXCEPTION));
+            throw new CategoryDescriptionException(DESCRIPTION_ARE_THE_SAME_EXCEPTION);
         }
 
-        Category category = findCategory(categoryTitle);
-        String actualDescription = category.getDescription();
-
-        if (newDescription.equals(actualDescription)) {
-            throw new CategoryDescriptionException("Categories descriptions are the same.");
-        }
-
-        category.setDescription(newDescription);
-
-        return CategoryMapper.buildCategoryDTO(category);
+        categoryToUpdate.setDescription(newDescription);
+        log.info("Category updated");
+        return CategoryMapper.buildCategoryDTO(categoryToUpdate);
     }
 
     @Override
-    public void deleteCategory(String categoryTitle) {
-        Category result = findCategory(categoryTitle);
-        repository.delete(result);
-    }
-
-    @Override
-    public Category findCategory(String categoryTitle) {
-        if (!repository.existsByTitle(categoryTitle)) {
-            throw new CategoryDoesntExistException("Category " + categoryTitle + " doesnt exist.");
-        }
-        return repository.findByTitle(categoryTitle);
+    public void deleteCategory(Long id) {
+        log.info("Deleting category with id {}", id);
+        Category categoryFound = findCategory(id);
+        repository.delete(categoryFound);
+        log.info("Deleted topic");
     }
 }
