4923271e2595dcd57f0f6b7796cdbf1ef3e7fe68
hubert <szabahubert@gmail.com>
2023-03-15 17:04:57 +0100

    added like mapper

---
+++ src/main/java/huberts/spring/forumapp/like/LikeMapper.java
@@ -0,0 +1,73 @@
+package huberts.spring.forumapp.like;
+
+import huberts.spring.forumapp.comment.Comment;
+import huberts.spring.forumapp.like.dto.LikeListDTO;
+import huberts.spring.forumapp.like.dto.LikeDTO;
+import huberts.spring.forumapp.topic.Topic;
+import huberts.spring.forumapp.user.User;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class LikeMapper {
+
+    public static Like buildTopicLike(User user, Topic topic) {
+        return Like.builder()
+                .user(user)
+                .topic(topic)
+                .build();
+    }
+
+    public static Like buildCommentLike(User user, Comment comment) {
+        return Like.builder()
+                .user(user)
+                .comment(comment)
+                .build();
+    }
+
+    public static LikeDTO buildLikeObjectDTO(Like like) {
+        String username = like.getUser().getUsername();
+        return LikeDTO.builder()
+                .id(like.getId())
+                .who(username)
+                .likedObjectId(getLikedObjectId(like))
+                .likedObject(getLikedObject(like))
+                .build();
+    }
+
+    private static Long getLikedObjectId(Like like) {
+        if (like.getTopic() != null)
+            return like.getTopic().getId();
+        else
+            return like.getComment().getId();
+    }
+
+    private static String getLikedObject(Like like) {
+        if (like.getTopic() != null)
+            return "Topic";
+        else
+            return "Comment";
+    }
+
+    public static LikeListDTO buildLikeDTO(Like like) {
+        String username = like.getUser().getUsername();
+        return LikeListDTO.builder()
+                .id(like.getId())
+                .who(username)
+                .build();
+    }
+
+    public static List<LikeDTO> mapLikesToLikeObjectDTO(List<Like> likes) {
+        return likes.stream()
+                .map(LikeMapper::buildLikeObjectDTO)
+                .collect(Collectors.toList());
+    }
+
+    public static List<LikeListDTO> mapLikesToLikeDTO(List<Like> likes) {
+        return likes.stream()
+                .map(LikeMapper::buildLikeDTO)
+                .collect(Collectors.toList());
+    }
+}
