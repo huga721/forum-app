7b4705618a21b51f39a831da366f0676f14ec19f
hubert <szabahubert@gmail.com>
2023-03-19 20:53:24 +0100

    added warning entity class

---
+++ src/main/java/huberts/spring/forumapp/warning/Warning.java
@@ -0,0 +1,19 @@
+package huberts.spring.forumapp.warning;
+
+import huberts.spring.forumapp.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Warning {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private User user;
+}
