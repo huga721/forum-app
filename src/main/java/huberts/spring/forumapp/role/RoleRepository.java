aef94b91d980300a755aa5d383cef0e29eda4fb9
hubert <szabahubert@gmail.com>
2023-03-27 21:07:48 +0200

    Deleted useless methods and refactoring.

--- src/main/java/huberts/spring/forumapp/role/RoleRepository.java
+++ src/main/java/huberts/spring/forumapp/role/RoleRepository.java
@@ -3,9 +3,10 @@ package huberts.spring.forumapp.role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-    Role findByCode(String code);
-    Boolean existsByName(String name);
+public interface
+RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
 }
