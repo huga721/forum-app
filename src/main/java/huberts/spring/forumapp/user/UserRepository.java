2b7a095787ba7889e336b554b0b10ee5721a9d4f
hubert <szabahubert@gmail.com>
2023-03-27 20:54:57 +0200

    Removed wrong used annotation and added optional type to findByUsername method.

--- src/main/java/huberts/spring/forumapp/user/UserRepository.java
+++ src/main/java/huberts/spring/forumapp/user/UserRepository.java
@@ -1,13 +1,12 @@
 package huberts.spring.forumapp.user;
 
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 
-@Transactional
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
     boolean existsByUsername(String username);
 }
