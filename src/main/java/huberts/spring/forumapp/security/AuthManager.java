8bafdf00e10c2ed3b371f9b736a4aaadc1721444
hubert <szabahubert@gmail.com>
2023-03-28 15:32:33 +0200

    Added validation to authentication manager.

--- src/main/java/huberts/spring/forumapp/security/AuthManager.java
+++ src/main/java/huberts/spring/forumapp/security/AuthManager.java
@@ -1,30 +1,50 @@
 package huberts.spring.forumapp.security;
 
-import huberts.spring.forumapp.exception.AccountBlockedException;
+import huberts.spring.forumapp.exception.user.AccountBlockedException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class AuthManager implements AuthenticationManager {
+
     private final DetailsService service;
+    private final PasswordEncoder passwordEncoder;
+
+    private final static String ACCOUNT_BLOCKED_EXCEPTION = "User is blocked.";
+    private final static String LOGIN_OR_PASSWORD_IS_WRONG = "Login or password is blank or empty.";
+    private final static String PASSWORD_IS_WRONG = "Password is wrong.";
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        UserDetails user = service.loadUserByUsername(authentication.getPrincipal().toString());
+        String username = authentication.getPrincipal().toString();
+        String password = authentication.getCredentials().toString();
 
-        if (!user.isAccountNonLocked()) {
-            throw new AccountBlockedException("User " + user.getUsername() + " is blocked.");
+        if (username.isBlank() || password.isBlank()) {
+            log.error("An exception occurred!", new BadCredentialsException(LOGIN_OR_PASSWORD_IS_WRONG));
+            throw new BadCredentialsException(LOGIN_OR_PASSWORD_IS_WRONG);
         }
 
-        log.info("username - {} | password - {}", user.getUsername(), user.getPassword());
+        UserDetails user = service.loadUserByUsername(username);
+
+        if (!passwordEncoder.matches(password, user.getPassword())){
+            log.error("An exception occurred!", new BadCredentialsException(PASSWORD_IS_WRONG));
+            throw new BadCredentialsException(PASSWORD_IS_WRONG);
+        }
+        if (!user.isAccountNonLocked()) {
+            log.error("An exception occurred!", new AccountBlockedException(ACCOUNT_BLOCKED_EXCEPTION));
+            throw new AccountBlockedException(ACCOUNT_BLOCKED_EXCEPTION);
+        }
+
         return new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword(), user.getAuthorities());
     }
 }
