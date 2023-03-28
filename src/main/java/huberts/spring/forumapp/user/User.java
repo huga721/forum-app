7df527bb826cdfee3ae78e98908806553c1cfadb
hubert <szabahubert@gmail.com>
2023-03-28 15:31:09 +0200

    Added new relations between entities and made white spaces between fields.

--- src/main/java/huberts/spring/forumapp/user/User.java
+++ src/main/java/huberts/spring/forumapp/user/User.java
@@ -1,34 +1,60 @@
 package huberts.spring.forumapp.user;
 
-import huberts.spring.forumapp.post.Post;
+import huberts.spring.forumapp.comment.Comment;
+import huberts.spring.forumapp.like.Like;
+import huberts.spring.forumapp.report.Report;
+import huberts.spring.forumapp.topic.Topic;
 import huberts.spring.forumapp.role.Role;
+import huberts.spring.forumapp.warning.Warning;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
 import java.util.List;
 
-@Entity(name = "`user`")
+@Entity(name = "users")
+@Getter
+@Setter
 @Builder
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(unique = true)
     private String username;
+
     @NotNull
     private String password;
+
     private boolean blocked;
+
     @OneToOne
-    @JoinColumn(referencedColumnName = "`name`", name = "`role_name`")
+    @JoinColumn(referencedColumnName = "name", name = "role_name")
     private Role role;
-    @OneToMany(mappedBy = "author")
-    List<Post> posts = new ArrayList<>();
+
+    @ManyToMany(mappedBy = "users", cascade = CascadeType.REMOVE)
+    private List<Topic> topics;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    private LocalDateTime lastActivity;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    private List<Like> likes;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    private List<Comment> comments;
+
+    @OneToOne(cascade = CascadeType.REMOVE)
+    private Report report;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    private List<Warning> warnings;
 }
