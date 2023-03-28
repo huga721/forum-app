b047d47fc96688def9e736a09b4a38eed92c36a4
hubert <szabahubert@gmail.com>
2023-03-28 22:13:53 +0200

    Added PropertySource annotation.

--- src/test/java/huberts/spring/forumapp/ContainerIT.java
+++ src/test/java/huberts/spring/forumapp/ContainerIT.java
@@ -3,12 +3,14 @@ package huberts.spring.forumapp;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @Testcontainers
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class ContainerIT {
 
     @Container
@@ -23,5 +25,6 @@ public class ContainerIT {
         registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
         registry.add("spring.datasource.username", mySQLContainer::getUsername);
         registry.add("spring.datasource.password", mySQLContainer::getPassword);
+        registry.add("spring.datasource.driverClassName", mySQLContainer::getDriverClassName);
     }
 }
