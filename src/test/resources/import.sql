56cb5766392b8f882653d5d656f7c0e22c89142e
hubert <szabahubert@gmail.com>
2023-03-29 19:48:07 +0200

    Added sql script file for initialising database of integration tests.

--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -2,16 +2,19 @@ INSERT INTO role (id, name, code) VALUES (1, 'ROLE_USER', '123');
 INSERT INTO role (id, name, code) VALUES (2, 'ROLE_MODERATOR', '123');
 INSERT INTO role (id, name, code) VALUES (3, 'ROLE_ADMIN', '123');
 
-INSERT INTO users (id, username, password, blocked, role_name) VALUES (1, 'userJwt', 'encrypted_password', false, 'ROLE_USER');
-INSERT INTO users (id, username, password, blocked, role_name) VALUES (2, 'adminJwt', 'encrypted_password', false, 'ROLE_ADMIN');
-INSERT INTO users (id, username, password, blocked, role_name) VALUES (3, 'moderatorJwt', 'encrypted_password', false, 'ROLE_MODERATOR');
-INSERT INTO users (id, username, password, blocked, role_name) VALUES (4, 'user', 'encrypted_password', false, 'ROLE_USER');
-INSERT INTO users (id, username, password, blocked, role_name) VALUES (5, 'userToChangePassword', 'encrypted_password', false, 'ROLE_USER');
-INSERT INTO users (id, username, password, blocked, role_name) VALUES (6, 'userToUnban', 'encrypted_password', true, 'ROLE_USER');
-INSERT INTO users (id, username, password, blocked, role_name) VALUES (7, 'userToCheckUnban', 'encrypted_password', false, 'ROLE_USER');
-INSERT INTO users (id, username, password, blocked, role_name) VALUES (8, 'userToBan', 'encrypted_password', false, 'ROLE_USER');
-INSERT INTO users (id, username, password, blocked, role_name) VALUES (9, 'userToCheckBan', 'encrypted_password', true, 'ROLE_USER');
-INSERT INTO users (id, username, password, blocked, role_name) VALUES (10, 'userToDelete', 'encrypted_password', false, 'ROLE_USER');
-INSERT INTO users (id, username, password, blocked, role_name) VALUES (11, 'userToDeleteByAdmin', 'encrypted_password', false, 'ROLE_USER');
-INSERT INTO users (id, username, password, blocked, role_name) VALUES (12, 'userToChangeRole', 'encrypted_password', false, 'ROLE_USER');
-INSERT INTO users (id, username, password, blocked, role_name) VALUES (13, 'userWithSameRole', 'encrypted_password', false, 'ROLE_USER');
+INSERT INTO users (id, username, password, blocked, role_name) VALUES (1, 'userJwt', '$2a$12$pXbWi0cGMnzxOddlT6RIGOYYGt9/IAdLU9Gkzeyvc13tudavD2RV2', false, 'ROLE_USER');
+INSERT INTO users (id, username, password, blocked, role_name) VALUES (2, 'adminJwt', '$2a$12$pXbWi0cGMnzxOddlT6RIGOYYGt9/IAdLU9Gkzeyvc13tudavD2RV2', false, 'ROLE_ADMIN');
+INSERT INTO users (id, username, password, blocked, role_name) VALUES (3, 'moderatorJwt', '$2a$12$pXbWi0cGMnzxOddlT6RIGOYYGt9/IAdLU9Gkzeyvc13tudavD2RV2', false, 'ROLE_MODERATOR');
+INSERT INTO users (id, username, password, blocked, role_name) VALUES (4, 'user', '$2a$12$pXbWi0cGMnzxOddlT6RIGOYYGt9/IAdLU9Gkzeyvc13tudavD2RV2', false, 'ROLE_USER');
+INSERT INTO users (id, username, password, blocked, role_name) VALUES (5, 'userToChangePassword', '$2a$12$pXbWi0cGMnzxOddlT6RIGOYYGt9/IAdLU9Gkzeyvc13tudavD2RV2', false, 'ROLE_USER');
+INSERT INTO users (id, username, password, blocked, role_name) VALUES (6, 'userToUnban', '$2a$12$pXbWi0cGMnzxOddlT6RIGOYYGt9/IAdLU9Gkzeyvc13tudavD2RV2', true, 'ROLE_USER');
+INSERT INTO users (id, username, password, blocked, role_name) VALUES (7, 'userToCheckUnban', '$2a$12$pXbWi0cGMnzxOddlT6RIGOYYGt9/IAdLU9Gkzeyvc13tudavD2RV2', false, 'ROLE_USER');
+INSERT INTO users (id, username, password, blocked, role_name) VALUES (8, 'userToBan', '$2a$12$pXbWi0cGMnzxOddlT6RIGOYYGt9/IAdLU9Gkzeyvc13tudavD2RV2', false, 'ROLE_USER');
+INSERT INTO users (id, username, password, blocked, role_name) VALUES (9, 'userToCheckBan', '$2a$12$pXbWi0cGMnzxOddlT6RIGOYYGt9/IAdLU9Gkzeyvc13tudavD2RV2', true, 'ROLE_USER');
+INSERT INTO users (id, username, password, blocked, role_name) VALUES (10, 'userToDelete', '$2a$12$pXbWi0cGMnzxOddlT6RIGOYYGt9/IAdLU9Gkzeyvc13tudavD2RV2', false, 'ROLE_USER');
+INSERT INTO users (id, username, password, blocked, role_name) VALUES (11, 'userToDeleteByAdmin', '$2a$12$pXbWi0cGMnzxOddlT6RIGOYYGt9/IAdLU9Gkzeyvc13tudavD2RV2', false, 'ROLE_USER');
+INSERT INTO users (id, username, password, blocked, role_name) VALUES (12, 'userToChangeRole', '$2a$12$pXbWi0cGMnzxOddlT6RIGOYYGt9/IAdLU9Gkzeyvc13tudavD2RV2', false, 'ROLE_USER');
+INSERT INTO users (id, username, password, blocked, role_name) VALUES (13, 'userWithSameRole', '$2a$12$pXbWi0cGMnzxOddlT6RIGOYYGt9/IAdLU9Gkzeyvc13tudavD2RV2', false, 'ROLE_USER');
+
+INSERT INTO users (id, username, password, blocked, role_name) VALUES (14, 'userBanned', '$2a$12$pXbWi0cGMnzxOddlT6RIGOYYGt9/IAdLU9Gkzeyvc13tudavD2RV2', true, 'ROLE_USER')
+-- INSERT INTO topic (id, title, content) VALUES (1, 'userJwt', 'encrypted_password');
