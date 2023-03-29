90933ed759bfa31630f6762ce379febe2885b856
hubert <szabahubert@gmail.com>
2023-03-29 20:00:11 +0200

    Deleted comments, logs and unnecessary this from constructor

--- src/main/java/huberts/spring/forumapp/security/filter/AuthFilter.java
+++ src/main/java/huberts/spring/forumapp/security/filter/AuthFilter.java
@@ -6,45 +6,36 @@ import huberts.spring.forumapp.security.AuthSuccessHandler;
 import huberts.spring.forumapp.user.dto.LoginDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.io.InputStream;
 
-@Slf4j
 @Component
 public class AuthFilter extends UsernamePasswordAuthenticationFilter {
+
     private final ObjectMapper mapper;
 
-    AuthFilter(ObjectMapper mapper, AuthManager manager, AuthSuccessHandler successHandler) {
+    public AuthFilter(ObjectMapper mapper, AuthManager manager, AuthSuccessHandler successHandler) {
         this.mapper = mapper;
-        this.setAuthenticationManager(manager);
-        this.setAuthenticationSuccessHandler(successHandler);
+        setAuthenticationManager(manager);
+        setAuthenticationSuccessHandler(successHandler);
     }
 
-    /**
-     * Method that performs authentication, reads data for request, then pass it to the authentication manager and
-     * authenticate it (search for the user in database)
-     */
     @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
         try {
-            // Reading the requested credentials.
-            LoginDTO login = mapper.readValue(request.getInputStream(), LoginDTO.class);
+            InputStream inputStream = request.getInputStream();
+            LoginDTO login = mapper.readValue(inputStream, LoginDTO.class);
 
-            log.info("\"attemptAuthentication\" method \n Login: {} | Password: {}", login.getUsername(), login.getPassword());
-
-            // Creating authentication token based on credentials.
-            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken
-                    (login.getUsername(), login.getPassword());
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                    login.getUsername(), login.getPassword());
 
             setDetails(request, token);
 
-            // Passing token to authentication manager to authenticate it.
             return this.getAuthenticationManager().authenticate(token);
         } catch (IOException e) {
             throw new RuntimeException(e);
