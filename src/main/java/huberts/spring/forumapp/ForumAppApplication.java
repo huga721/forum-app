338fbc5da9e208c55ad570fd58395463c3b31730
hubert <szabahubert@gmail.com>
2023-04-01 16:12:34 +0200

    Deleted commented bean.

--- src/main/java/huberts/spring/forumapp/ForumAppApplication.java
+++ src/main/java/huberts/spring/forumapp/ForumAppApplication.java
@@ -1,10 +1,5 @@
 package huberts.spring.forumapp;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,13 +9,4 @@ public class ForumAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(ForumAppApplication.class, args);
     }
-
-//        @Bean
-//        CommandLineRunner commandLineRunner(RoleService roleService) {
-//            return args -> {
-//                roleService.addRole(new Role(1, "ROLE_USER", "123U"));
-//                roleService.addRole(new Role(2, "ROLE_MODERATOR", "456M"));
-//                roleService.addRole(new Role(3, "ROLE_ADMIN", "789A"));
-//            };
-//        }
 }
