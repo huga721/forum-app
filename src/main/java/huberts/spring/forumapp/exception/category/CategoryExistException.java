e65a7bd00f193c3364124a6e2dab9ebcc12c63c8
hubert <szabahubert@gmail.com>
2023-04-01 16:16:35 +0200

    Changed exception class name.

--- src/main/java/huberts/spring/forumapp/exception/category/CategoryExistException.java
+++
@@ -1,7 +0,0 @@
-package huberts.spring.forumapp.exception.category;
-
-public class CategoryExistException extends RuntimeException{
-    public CategoryExistException(String message) {
-        super(message);
-    }
-}
