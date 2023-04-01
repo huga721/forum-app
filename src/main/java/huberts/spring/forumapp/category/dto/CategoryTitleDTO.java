2aed8994277aa937d60a909f2649d3abb0e390fa
hubert <szabahubert@gmail.com>
2023-04-01 16:09:57 +0200

    Added constructor annotations for testing purposes.

--- src/main/java/huberts/spring/forumapp/category/dto/CategoryTitleDTO.java
+++ src/main/java/huberts/spring/forumapp/category/dto/CategoryTitleDTO.java
@@ -1,11 +1,13 @@
 package huberts.spring.forumapp.category.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoryTitleDTO {
     @NotBlank(message = "Title to change can't be blank.")
     private String categoryTitle;
--- src/main/java/huberts/spring/forumapp/topic/dto/TopicCreateDTO.java
+++ src/main/java/huberts/spring/forumapp/topic/dto/TopicCreateDTO.java
@@ -2,9 +2,13 @@ package huberts.spring.forumapp.topic.dto;
 
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TopicCreateDTO {
     @NotBlank(message = "Topic title can't be blank.")
     private String title;
