14f1a7e960053961bb97db29d46f0b1e8a91560b
hubert <szabahubert@gmail.com>
2023-04-02 20:50:24 +0200

    Added mapping of topic method.

--- src/main/java/huberts/spring/forumapp/user/UserMapper.java
+++ src/main/java/huberts/spring/forumapp/user/UserMapper.java
@@ -1,9 +1,14 @@
 package huberts.spring.forumapp.user;
 
 import huberts.spring.forumapp.role.Role;
+import huberts.spring.forumapp.topic.Topic;
+import huberts.spring.forumapp.topic.TopicMapper;
+import huberts.spring.forumapp.topic.dto.ShortTopicDTO;
 import huberts.spring.forumapp.user.dto.UserDTO;
+import huberts.spring.forumapp.warning.Warning;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,27 +16,36 @@ import java.util.stream.Collectors;
 @Component
 public class UserMapper {
 
-    User buildUser(String username, String password, Role role) {
+    public static User buildUser(String username, String password, Role role) {
+        List<Topic> emptyList = new ArrayList<>();
+        List<Warning> emptyWarnings = new ArrayList<>();
         return User.builder()
                 .username(username)
                 .password(password)
                 .role(role)
                 .blocked(false)
+                .topics(emptyList)
+                .warnings(emptyWarnings)
                 .build();
     }
 
-    List<UserDTO> mapFromList (List<User> listOfUser) {
-        return listOfUser.stream()
-                .map(this::buildUserDTO)
+    public static List<UserDTO> mapFromList (List<User> users) {
+        return users.stream()
+                .map(UserMapper::buildUserDTO)
                 .collect(Collectors.toList());
     }
 
-    UserDTO buildUserDTO(User user) {
+    public static UserDTO buildUserDTO(User user) {
         return UserDTO.builder()
                 .username(user.getUsername())
                 .role(user.getRole().getName())
-                .postActivity(user.getPosts())
+                .topics(mapToTopicDTO(user.getTopics()))
+                .warningPoints(user.getWarnings().size() * 20)
                 .blocked(user.isBlocked())
                 .build();
+    }
+
+    private static List<ShortTopicDTO> mapToTopicDTO(List<Topic> topics) {
+        return TopicMapper.mapShortTopicDTOList(topics);
     }
 }
