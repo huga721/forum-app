31c4198ddc4048e3b7b8b62d0ab4e8463999584f
hubert <szabahubert@gmail.com>
2023-04-03 21:51:43 +0200

    Added new fields.

--- src/main/java/huberts/spring/forumapp/topic/dto/TopicDTO.java
+++ src/main/java/huberts/spring/forumapp/topic/dto/TopicDTO.java
@@ -1,17 +1,24 @@
 package huberts.spring.forumapp.topic.dto;
 
+import huberts.spring.forumapp.comment.dto.CommentDTO;
+import huberts.spring.forumapp.like.dto.LikeListDTO;
 import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Builder
 public class TopicDTO {
+    private Long id;
     private String title;
     private String content;
     private String author;
     private String categoryName;
+    private boolean isClosed;
     private LocalDateTime createdTime;
     private LocalDateTime lastEdit;
+    private List<LikeListDTO> likes;
+    private List<CommentDTO> comments;
 }
