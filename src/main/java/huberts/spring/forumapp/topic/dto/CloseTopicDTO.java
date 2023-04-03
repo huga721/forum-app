36cee27b29070c11768a693df1ebe235810297a5
hubert <szabahubert@gmail.com>
2023-04-03 21:57:31 +0200

    Added model class for closing topic logic.

---
+++ src/main/java/huberts/spring/forumapp/topic/dto/CloseTopicDTO.java
@@ -0,0 +1,14 @@
+package huberts.spring.forumapp.topic.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CloseTopicDTO {
+    @NotBlank(message = "Please write reason of closing topic")
+    private String reason;
+}
