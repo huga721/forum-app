de1ca4d0ffdb61ca2d0c5733d006825d4e651806
hubert <szabahubert@gmail.com>
2023-04-03 21:44:33 +0200

    Deleted unused jpa methods.

--- src/main/java/huberts/spring/forumapp/topic/TopicRepository.java
+++ src/main/java/huberts/spring/forumapp/topic/TopicRepository.java
@@ -5,17 +5,10 @@ import huberts.spring.forumapp.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TopicRepository extends JpaRepository<Topic, Long> {
-    Optional<Topic> findByTitle(String title);
-    Optional<Topic> findByUsersAndId(User user, Long id);
-    Optional<Topic> findByTitleAndCategories(String title, Category category);
-    boolean existsByTitle(String title);
-    boolean existsByTitleAndUsersAndCategories(String title, User user, Category category);
-    List<Topic> findAllByTitle(String title);
-    Topic findByTitleAndCategoriesAndUsers(String title, Category category, User user);
+    Optional<Topic> findByUserAndId(User user, Long id);
+    boolean existsByTitleAndUserAndCategory(String title, User user, Category category);
 }
