db2124ccebebf638264631d7dcdfc53a241a1b1b
hubert <szabahubert@gmail.com>
2023-04-12 09:56:54 +0200

    Update SecurityController to include validation and improved method names also deleted Log annotation and messages.

--- src/main/java/huberts/spring/forumapp/security/SecurityController.java
+++ src/main/java/huberts/spring/forumapp/security/SecurityController.java
@@ -1,29 +1,28 @@
 package huberts.spring.forumapp.security;
 
-import huberts.spring.forumapp.user.UserService;
+import huberts.spring.forumapp.user.service.UserService;
 import huberts.spring.forumapp.user.dto.LoginDTO;
 import huberts.spring.forumapp.user.dto.RegisterDTO;
 import huberts.spring.forumapp.user.dto.UserDTO;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
-
+@Validated
 @RestController
-@Slf4j
 @RequiredArgsConstructor
 public class SecurityController {
     private final UserService userService;
 
     @PostMapping("/register")
-    ResponseEntity<UserDTO> registerUser (@RequestBody RegisterDTO register) {
-        log.info("SecurityController call \'/register\' - endpoint");
-        return ResponseEntity.created(URI.create("/" + register.getUsername())).body(userService.addUser(register));
+    ResponseEntity<UserDTO> registerUser (@RequestBody @Valid RegisterDTO register) {
+        return ResponseEntity.created(URI.create("/" + register.getUsername())).body(userService.createUser(register));
     }
     @PostMapping("/login")
     ResponseEntity<Void> loginUser (@RequestBody LoginDTO credentials) {
