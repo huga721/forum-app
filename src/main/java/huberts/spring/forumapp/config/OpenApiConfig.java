8c5390a4d96303fe5c6e1dc9190192798d946209
hubert <szabahubert@gmail.com>
2023-04-12 09:58:54 +0200

    Moved OpenApiConfig class to config package for better organization.

--- src/main/java/huberts/spring/forumapp/config/OpenApiConfig.java
+++ src/main/java/huberts/spring/forumapp/config/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package huberts.spring.forumapp.security;
+package huberts.spring.forumapp.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
