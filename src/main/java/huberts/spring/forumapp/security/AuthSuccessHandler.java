dfaf0ec95f21ea90066c78e21285b670b99db30b
hubert <szabahubert@gmail.com>
2023-04-20 16:39:30 +0200

    Added logs, deleted comment and refactored code.

--- src/main/java/huberts/spring/forumapp/security/AuthSuccessHandler.java
+++ src/main/java/huberts/spring/forumapp/security/AuthSuccessHandler.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.core.Authentication;
@@ -18,11 +19,12 @@ import java.util.Map;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-@PropertySource("classpath:/customAppProperties.properties")
 @Component
+@PropertySource("classpath:/customAppProperties.properties")
+@Slf4j
 public class AuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-    private int expirationTime;
+    private final int expirationTime;
     private final String secret;
 
     public AuthSuccessHandler(@Value("${spring.custom.jwt.expiration}") int expirationTime,
@@ -31,22 +33,20 @@ public class AuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
         this.secret = secret;
     }
 
-    /**
-     * Creates token on succeed authentication.
-     */
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException {
-
-        var user = authentication.getPrincipal();
+        Object user = authentication.getPrincipal();
 
         String token = JWT.create()
                 .withSubject(user.toString())
                 .withExpiresAt(new Date(System.currentTimeMillis() + expirationTime))
                 .sign(Algorithm.HMAC256(secret));
 
+        log.info("Authentication success, generating JWT token.");
         Map<String, String> content = new HashMap<>();
         content.put("token", token);
+
         response.setContentType(APPLICATION_JSON_VALUE);
         new ObjectMapper().writeValue(response.getOutputStream(), content);
     }
