abd7fe7f6f4dc3363a4b96d9baacb3fdd8d8a6b3
hubert <szabahubert@gmail.com>
2023-04-30 21:28:18 +0200

    Refactored code, added methods from utility class to validate Topic.

--- src/main/java/huberts/spring/forumapp/report/ReportService.java
+++ src/main/java/huberts/spring/forumapp/report/ReportService.java
@@ -2,23 +2,22 @@ package huberts.spring.forumapp.report;
 
 import huberts.spring.forumapp.comment.Comment;
 import huberts.spring.forumapp.comment.CommentRepository;
-import huberts.spring.forumapp.exception.comment.CommentExistException;
-import huberts.spring.forumapp.exception.report.ReportExistException;
-import huberts.spring.forumapp.exception.report.ReportRealiseException;
-import huberts.spring.forumapp.exception.topic.TopicExistException;
+import huberts.spring.forumapp.exception.comment.CommentDoesntExistException;
+import huberts.spring.forumapp.exception.report.ReportDoesntExistException;
+import huberts.spring.forumapp.exception.topic.TopicDoesntExistException;
 import huberts.spring.forumapp.report.dto.ReportDTO;
 import huberts.spring.forumapp.report.dto.ReportReasonDTO;
 import huberts.spring.forumapp.topic.Topic;
 import huberts.spring.forumapp.topic.TopicRepository;
 import huberts.spring.forumapp.user.User;
 import huberts.spring.forumapp.user.UserRepository;
+import huberts.spring.forumapp.utility.UtilityService;
 import huberts.spring.forumapp.warning.WarningService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -30,112 +29,118 @@ public class ReportService implements ReportServiceApi {
     private final ReportRepository reportRepository;
     private final UserRepository userRepository;
     private final WarningService warningService;
+    private final UtilityService utilityService;
 
     private static final String COMMENT_DOESNT_EXIST_EXCEPTION = "Comment with id \"%d\" doesn't exist.";
     private static final String TOPIC_DOESNT_EXIST_EXCEPTION = "Topic with id \"%d\" doesn't exist.";
     private static final String REPORT_DOESNT_EXIST_EXCEPTION = "Report with id \"%d\" doesn't exist.";
-    private static final String REPORT_REALISE_EXIST_EXCEPTION = "Can't realise reports because there are less than 5 reports.";
     private static final String EXCEPTION_OCCURRED = "An exception occurred!";
 
     @Override
-    public ReportDTO createCommentReport(Long id, ReportReasonDTO reasonDTO, String username) {
-        log.info("Creating a report for comment with id {}", id);
-        User userReporting = userRepository.findByUsername(username).get();
-        Comment commentFound = findCommentById(id);
+    public ReportDTO createCommentReport(Long commentId, ReportReasonDTO reportReasonDTO, String username) {
+        log.info("Creating a report for comment with id {}", commentId);
+        User user = findUserByUsername(username);
+        Comment commentFound = findCommentById(commentId);
 
-        String reason = reasonDTO.getReason();
-
-        Report reportBuilt = ReportMapper.buildReport(userReporting, reason, commentFound);
-        Report reportSaved = reportRepository.save(reportBuilt);
+        utilityService.validateTopicClosed(commentFound.getTopic());
+        utilityService.updateUserLastActivity(username);
         log.info("Report created");
-        return ReportMapper.buildReportTopicDTO(reportSaved);
+        return buildAndSaveReport(user, reportReasonDTO, commentFound);
     }
 
-    private Comment findCommentById(Long id) {
-        log.info("Finding comment with id {}", id);
-        return commentRepository.findById(id)
+    private ReportDTO buildAndSaveReport(User user, ReportReasonDTO reportReasonDTO, Comment comment) {
+        Report reportBuilt = ReportMapper.buildReport(user, reportReasonDTO.reason(), comment);
+        reportRepository.save(reportBuilt);
+        return ReportMapper.buildReportTopicDTO(reportBuilt);
+    }
+
+    private User findUserByUsername(String username) {
+        return userRepository.findByUsername(username).orElseThrow();
+    }
+
+    private Comment findCommentById(Long commentId) {
+        log.info("Finding comment with id {}", commentId);
+        return commentRepository.findById(commentId)
                 .orElseThrow(() -> {
-                    String errorMessage = String.format(COMMENT_DOESNT_EXIST_EXCEPTION, id);
-                    log.error(EXCEPTION_OCCURRED, new CommentExistException(errorMessage));
-                    return new CommentExistException(errorMessage);
+                    String errorMessage = String.format(COMMENT_DOESNT_EXIST_EXCEPTION, commentId);
+                    log.error(EXCEPTION_OCCURRED, new CommentDoesntExistException(errorMessage));
+                    throw new CommentDoesntExistException(errorMessage);
                 });
     }
 
     @Override
-    public ReportDTO createTopicReport(Long id, ReportReasonDTO reasonDTO, String username) {
-        log.info("Creating a report for topic with id {}", id);
-        User userReporting = userRepository.findByUsername(username).get();
-        Topic topicFound = findTopicById(id);
+    public ReportDTO createTopicReport(Long topicId, ReportReasonDTO reportReasonDTO, String username) {
+        log.info("Creating a report for topic with id {}", topicId);
+        User user = findUserByUsername(username);
+        Topic topicFound = findTopicById(topicId);
 
-        String reason = reasonDTO.getReason();
-
-        Report reportBuilt = ReportMapper.buildReport(userReporting, reason, topicFound);
-        Report reportSaved = reportRepository.save(reportBuilt);
-
+        utilityService.validateTopicClosed(topicFound);
+        utilityService.updateUserLastActivity(username);
         log.info("Report created");
-        return ReportMapper.buildReportTopicDTO(reportSaved);
+        return buildAndSaveReport(user, reportReasonDTO, topicFound);
     }
 
-    private Topic findTopicById(Long id) {
-        log.info("Finding topic with id {}", id);
-        return topicRepository.findById(id)
+    private ReportDTO buildAndSaveReport(User user, ReportReasonDTO reportReasonDTO, Topic topic) {
+        Report reportBuilt = ReportMapper.buildReport(user, reportReasonDTO.reason(), topic);
+        reportRepository.save(reportBuilt);
+        return ReportMapper.buildReportTopicDTO(reportBuilt);
+    }
+
+    private Topic findTopicById(Long topicId) {
+        log.info("Finding topic with id {}", topicId);
+        return topicRepository.findById(topicId)
                 .orElseThrow(() -> {
-                    String errorMessage = String.format(TOPIC_DOESNT_EXIST_EXCEPTION, id);
-                    log.error(EXCEPTION_OCCURRED, new TopicExistException(errorMessage));
-                    return new TopicExistException(errorMessage);
+                    String errorMessage = String.format(TOPIC_DOESNT_EXIST_EXCEPTION, topicId);
+                    log.error(EXCEPTION_OCCURRED, new TopicDoesntExistException(errorMessage));
+                    throw new TopicDoesntExistException(errorMessage);
                 });
     }
 
     @Override
-    public ReportDTO getReportById(Long id) {
-        log.info("Getting a report with id {}", id);
-        return ReportMapper.buildReportTopicDTO(findReport(id));
+    public ReportDTO getReportById(Long reportId) {
+        log.info("Getting a report with id {}", reportId);
+        return ReportMapper.buildReportTopicDTO(findReportById(reportId));
     }
 
-    private Report findReport(Long id) {
-        log.info("Finding report with id {}", id);
-        return reportRepository.findById(id)
+    private Report findReportById(Long reportId) {
+        log.info("Finding report with id {}", reportId);
+        return reportRepository.findById(reportId)
                 .orElseThrow(() -> {
-                    String errorMessage = String.format(REPORT_DOESNT_EXIST_EXCEPTION, id);
-                    log.error(EXCEPTION_OCCURRED, new ReportExistException(errorMessage));
-                    return new ReportExistException(errorMessage);
+                    String errorMessage = String.format(REPORT_DOESNT_EXIST_EXCEPTION, reportId);
+                    log.error(EXCEPTION_OCCURRED, new ReportDoesntExistException(errorMessage));
+                    throw new ReportDoesntExistException(errorMessage);
                 });
     }
 
     @Override
     public List<ReportDTO> getAllReports() {
         log.info("Getting all reports");
-        return ReportMapper.mapReportToDTO(reportRepository.findAll());
+        return ReportMapper.mapReportsToDTOList(reportRepository.findAll());
     }
 
     @Override
     public List<ReportDTO> getAllNotSeenReports() {
         log.info("Getting all not seen reports");
-        return ReportMapper.mapReportToDTO(reportRepository.findAll()).stream()
-                .filter(reportDTO -> !reportDTO.isSeen())
-                .collect(Collectors.toList());
+        return ReportMapper.mapReportsToNotSeenDTOList(reportRepository.findAll());
     }
 
     @Override
-    public ReportDTO markReportAsSeen(Long id) {
-        log.info("Setting a report with id {} as seen", id);
-        Report foundReport = findReport(id);
-        foundReport.setSeen(true);
+    public ReportDTO markReportAsSeen(Long reportId) {
+        log.info("Setting a report with id {} as seen", reportId);
+        Report reportFound = findReportById(reportId);
+        reportFound.setSeen(true);
         log.info("Report set");
-        return ReportMapper.buildReportTopicDTO(foundReport);
+        return ReportMapper.buildReportTopicDTO(reportFound);
     }
 
     @Override
-    public void executeReportAndWarnTopicAuthor(Long id) {
-        log.info("Deleting all reports for topic with id {} and warning author of topic", id);
-        Topic topicFound = findTopicById(id);
+    public void executeReportAndWarnTopicAuthor(Long topicId) {
+        log.info("Deleting all reports of topic and deleting topic with id {} and warning author of topic", topicId);
+        Topic topicFound = findTopicById(topicId);
+        String author = topicFound.getUser().getUsername();
 
-        if (topicFound.getReports().size() < 5) {
-            log.error(REPORT_REALISE_EXIST_EXCEPTION);
-            throw new ReportRealiseException(REPORT_REALISE_EXIST_EXCEPTION);
-        }
-
-        String author = topicFound.getUsers().get(0).getUsername();
+        utilityService.validateTopicClosed(topicFound);
+        utilityService.validateReports(topicFound.getReports());
 
         topicRepository.delete(topicFound);
         warningService.giveWarning(author);
@@ -143,16 +148,13 @@ public class ReportService implements ReportServiceApi {
     }
 
     @Override
-    public void executeReportAndWarnCommentAuthor(Long id) {
-        log.info("Deleting all reports for comment with id {} and warning author of comment", id);
-        Comment commentFound = findCommentById(id);
-
-        if (commentFound.getReports().size() < 5) {
-            log.error(REPORT_REALISE_EXIST_EXCEPTION);
-            throw new ReportRealiseException(REPORT_REALISE_EXIST_EXCEPTION);
-        }
-
+    public void executeReportAndWarnCommentAuthor(Long commentId) {
+        log.info("Deleting all reports for comment with id {} and warning author of comment", commentId);
+        Comment commentFound = findCommentById(commentId);
         String author = commentFound.getUser().getUsername();
+
+        utilityService.validateTopicClosed(commentFound.getTopic());
+        utilityService.validateReports(commentFound.getReports());
 
         commentRepository.delete(commentFound);
         warningService.giveWarning(author);
@@ -160,9 +162,9 @@ public class ReportService implements ReportServiceApi {
     }
 
     @Override
-    public void deleteReport(Long id) {
-        log.info("Deleting report with id {}", id);
-        Report reportFound = findReport(id);
+    public void deleteReport(Long reportId) {
+        log.info("Deleting report with id {}", reportId);
+        Report reportFound = findReportById(reportId);
         reportRepository.delete(reportFound);
         log.info("Report deleted");
     }
