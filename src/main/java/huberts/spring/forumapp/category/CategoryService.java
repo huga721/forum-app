1f00501d9518cd9ce9c784a99621f5654c3b666f
hubert <szabahubert@gmail.com>
2023-05-01 12:51:17 +0200

    Refactored parameter names, refactored methods.

--- src/main/java/huberts/spring/forumapp/category/CategoryService.java
+++ src/main/java/huberts/spring/forumapp/category/CategoryService.java
@@ -1,9 +1,6 @@
 package huberts.spring.forumapp.category;
 
-import huberts.spring.forumapp.category.dto.CategoryDTO;
-import huberts.spring.forumapp.category.dto.CategoryCreateDTO;
-import huberts.spring.forumapp.category.dto.CategoryDescriptionDTO;
-import huberts.spring.forumapp.category.dto.CategoryTitleDTO;
+import huberts.spring.forumapp.category.dto.*;
 import huberts.spring.forumapp.exception.category.CategoryAlreadyExistException;
 import huberts.spring.forumapp.exception.category.CategoryDescriptionException;
 import huberts.spring.forumapp.exception.category.CategoryDoesntExistException;
@@ -21,44 +18,45 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CategoryService implements CategoryServiceApi {
 
-    private final CategoryRepository repository;
-
     private static final String CATEGORY_DOESNT_EXIST_EXCEPTION = "Category with title \"%s\" doesn't exist.";
     private static final String CATEGORY_ALREADY_EXISTS_EXCEPTION = "Category \"%s\" already exists, please choose another title.";
     private static final String TITLE_IS_THE_SAME_EXCEPTION = "New title is the same as actual title in category.";
     private static final String DESCRIPTION_ARE_THE_SAME_EXCEPTION = "New description is the same as actual description in category.";
     private static final String EXCEPTION_OCCURRED = "An exception occurred!";
 
-    @Override
-    public CategoryDTO createCategory(CategoryCreateDTO category) {
-        String title = category.getTitle();
-        log.info("Creating category with title {}", title);
+    private final CategoryRepository repository;
 
-        if (repository.existsByTitle(title)) {
-            String errorMessage = String.format(CATEGORY_ALREADY_EXISTS_EXCEPTION, title);
+    @Override
+    public CategoryDTO createCategory(CreateCategoryDTO createCategoryDTO) {
+        log.info("Creating category with title {}", createCategoryDTO.title());
+        if (repository.existsByTitle(createCategoryDTO.title())) {
+            String errorMessage = String.format(CATEGORY_ALREADY_EXISTS_EXCEPTION, createCategoryDTO.title());
             log.error(EXCEPTION_OCCURRED, new CategoryAlreadyExistException(errorMessage));
             throw new CategoryAlreadyExistException(String.format(errorMessage));
         }
-
-        Category categoryBuilt = CategoryMapper.buildNewCategory(category);
-        repository.save(categoryBuilt);
-
         log.info("Category created");
+        return buildAndSaveCategory(createCategoryDTO);
+    }
+
+    private CategoryDTO buildAndSaveCategory(CreateCategoryDTO createCategoryDTO) {
+        Category categoryBuilt = CategoryMapper.buildNewCategory(createCategoryDTO);
+        repository.save(categoryBuilt);
         return CategoryMapper.buildCategoryDTO(categoryBuilt);
+
     }
 
     @Override
-    public CategoryDTO getCategoryById(Long id) {
-        log.info("Getting category with id {}", id);
-        Category categoryFound = findCategory(id);
+    public CategoryDTO getCategoryById(Long categoryId) {
+        log.info("Getting category with id {}", categoryId);
+        Category categoryFound = findCategoryById(categoryId);
         return CategoryMapper.buildCategoryDTO(categoryFound);
     }
 
-    private Category findCategory(Long id) {
-        log.info("Finding category with id {}", id);
-        return repository.findById(id)
+    private Category findCategoryById(Long categoryId) {
+        log.info("Finding category with id {}", categoryId);
+        return repository.findById(categoryId)
                 .orElseThrow(() -> {
-                    String errorMessage = String.format(CATEGORY_DOESNT_EXIST_EXCEPTION, id);
+                    String errorMessage = String.format(CATEGORY_DOESNT_EXIST_EXCEPTION, categoryId);
                     log.error(EXCEPTION_OCCURRED, new CategoryDoesntExistException(errorMessage));
                     throw new CategoryDoesntExistException(errorMessage);
                 });
@@ -71,14 +69,13 @@ public class CategoryService implements CategoryServiceApi {
     }
 
     @Override
-    public CategoryDTO updateTitle(Long id, CategoryTitleDTO newTitleDTO) {
-        log.info("Updating title of category with id {} by moderator or admin", id);
-        String newTitle = newTitleDTO.getCategoryTitle();
+    public CategoryDTO updateTitle(Long categoryId, NewCategoryTitleDTO newCategoryTitleDTO) {
+        log.info("Updating title of category with id {} by moderator or admin", categoryId);
+        Category categoryFound = findCategoryById(categoryId);
+        String title = categoryFound.getTitle();
+        String newTitle = newCategoryTitleDTO.title();
 
-        Category categoryToUpdate = findCategory(id);
-        String categoryTitle = categoryToUpdate.getTitle();
-
-        if (categoryTitle.equals(newTitle)) {
+        if (title.equals(newTitle)) {
             log.error(EXCEPTION_OCCURRED, new CategoryTitleException(TITLE_IS_THE_SAME_EXCEPTION));
             throw new CategoryTitleException(TITLE_IS_THE_SAME_EXCEPTION);
         }
@@ -88,33 +85,32 @@ public class CategoryService implements CategoryServiceApi {
             throw new CategoryTitleException(String.format(errorMessage));
         }
 
-        categoryToUpdate.setTitle(newTitle);
+        categoryFound.setTitle(newTitle);
         log.info("Category updated");
-        return CategoryMapper.buildCategoryDTO(categoryToUpdate);
+        return CategoryMapper.buildCategoryDTO(categoryFound);
     }
 
     @Override
-    public CategoryDTO updateDescription(Long id, CategoryDescriptionDTO newCategoryDescription) {
-        log.info("Updating description of category with id {} by moderator or admin", id);
-        String newDescription = newCategoryDescription.getDescription();
+    public CategoryDTO updateDescription(Long categoryId, NewCategoryDescriptionDTO newCategoryDescriptionDTO) {
+        log.info("Updating description of category with id {} by moderator or admin", categoryId);
+        Category categoryFound = findCategoryById(categoryId);
+        String description = categoryFound.getDescription();
+        String newDescription = newCategoryDescriptionDTO.description();
 
-        Category categoryToUpdate = findCategory(id);
-        String categoryDescription = categoryToUpdate.getDescription();
-
-        if (newDescription.equals(categoryDescription)) {
+        if (newDescription.equals(description)) {
             log.error(EXCEPTION_OCCURRED, new CategoryDescriptionException(DESCRIPTION_ARE_THE_SAME_EXCEPTION));
             throw new CategoryDescriptionException(DESCRIPTION_ARE_THE_SAME_EXCEPTION);
         }
 
-        categoryToUpdate.setDescription(newDescription);
+        categoryFound.setDescription(newDescription);
         log.info("Category updated");
-        return CategoryMapper.buildCategoryDTO(categoryToUpdate);
+        return CategoryMapper.buildCategoryDTO(categoryFound);
     }
 
     @Override
-    public void deleteCategory(Long id) {
-        log.info("Deleting category with id {}", id);
-        Category categoryFound = findCategory(id);
+    public void deleteCategory(Long categoryId) {
+        log.info("Deleting category with id {}", categoryId);
+        Category categoryFound = findCategoryById(categoryId);
         repository.delete(categoryFound);
         log.info("Deleted topic");
     }
