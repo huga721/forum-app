c7247a38ef36d8136792642e312a147c20368ca3
huga721 <szabahubert@gmail.com>
2023-05-18 18:55:56 +0200

    Renamed from RegisterDTO.

--- src/main/java/huberts/spring/forumapp/user/dto/CredentialsDTO.java
+++ src/main/java/huberts/spring/forumapp/user/dto/CredentialsDTO.java
@@ -2,7 +2,7 @@ package huberts.spring.forumapp.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record RegisterDTO(
+public record CredentialsDTO(
         @NotBlank(message = "Username can't be blank.") String username,
         @NotBlank(message = "Password can't be blank.") String password) {
 }
