89bcb73b1f154e150321f69d0a949f4558dfbccf
huga721 <szabahubert@gmail.com>
2023-06-01 17:10:32 +0200

    Code refactor and created utility class for createWarning method.

--- src/main/java/huberts/spring/forumapp/warning/WarningService.java
+++ src/main/java/huberts/spring/forumapp/warning/WarningService.java
@@ -2,12 +2,10 @@ package huberts.spring.forumapp.warning;
 
 import huberts.spring.forumapp.exception.user.UserBlockException;
 import huberts.spring.forumapp.exception.user.UserDoesntExistException;
-import huberts.spring.forumapp.exception.warning.WarningExistException;
+import huberts.spring.forumapp.exception.warning.WarningDoesntExistException;
 import huberts.spring.forumapp.user.User;
 import huberts.spring.forumapp.user.UserRepository;
-import huberts.spring.forumapp.user.service.UserService;
 import huberts.spring.forumapp.warning.dto.WarningDTO;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,13 +14,8 @@ import java.util.List;
 
 @Slf4j
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class WarningService implements WarningServiceApi {
-
-    private final WarningRepository warningRepository;
-    private final UserService userService;
-    private final UserRepository userRepository;
 
     private static final String USER_DOESNT_EXIST_EXCEPTION = "User \"%s\" doesn't exist.";
     private static final String USER_BANNED_EXCEPTION = "User \"%s\" is banned.";
@@ -30,26 +23,40 @@ public class WarningService implements WarningServiceApi {
     private static final String WARNING_DOESNT_EXIST_EXCEPTION = "Warning with id \"%d\" doesn't exist.";
     private static final String EXCEPTION_OCCURRED = "An exception occurred!";
 
+    private final WarningRepository warningRepository;
+    private final UserRepository userRepository;
+
     @Override
-    public WarningDTO giveWarning(String username) {
-        User userWarned = findByUsername(username);
+    public WarningDTO createWarning(String username) {
         log.info("Creating a warning for a user with username {}", username);
-        if (userWarned.isBlocked()) {
-            String errorMessage = String.format(USER_BANNED_EXCEPTION, username);
+        User user = findUserByUsername(username);
+        validateUserBlocked(user);
+        log.info("Warning created");
+        return buildAndSaveWarning(user);
+    }
+
+    public WarningDTO buildAndSaveWarning(User user) {
+        Warning warning = WarningMapper.buildWarning(user);
+        log.info("BEFORE SAVE = {}", user.getWarnings().size());
+        warningRepository.save(warning);
+        user.getWarnings().add(warning);
+        log.info("AFTER SAVE = {}", user.getWarnings().size());
+        if (user.getWarnings().size() >= 5) {
+            log.debug("Blocking user {} because number of his/her warnings is 5", user.getUsername());
+            user.setBlocked(true);
+        }
+        return WarningMapper.buildWarningDTO(warning);
+    }
+
+    private void validateUserBlocked(User user) {
+        if (user.isBlocked()) {
+            String errorMessage = String.format(USER_BANNED_EXCEPTION, user.getUsername());
             log.error(EXCEPTION_OCCURRED, new UserBlockException(errorMessage));
             throw new UserBlockException(errorMessage);
         }
-        Warning warningBuilt = WarningMapper.buildWarning(userWarned);
-        Warning warningSaved = warningRepository.save(warningBuilt);
-        if (userWarned.getWarnings().size() == 5) {
-            log.debug("Blocking user {} because number of his/her warnings is 5", username);
-            userService.banUser(username);
-        }
-        log.info("Warning created");
-        return WarningMapper.buildWarningDTO(warningSaved);
     }
 
-    private User findByUsername(String username) {
+    private User findUserByUsername(String username) {
         log.info("Fining user with username {}", username);
         return userRepository.findByUsername(username)
                 .orElseThrow(() -> {
@@ -60,38 +67,35 @@ public class WarningService implements WarningServiceApi {
     }
 
     @Override
-    public WarningDTO getWarningById(Long id) {
-        log.info("Getting warning by id {}", id);
-        return WarningMapper.buildWarningDTO(warningRepository.findById(id)
+    public WarningDTO getWarningById(Long warningId) {
+        log.info("Getting warning by id {}", warningId);
+        return WarningMapper.buildWarningDTO(warningRepository.findById(warningId)
                 .orElseThrow(() -> {
-                    String errorMessage = String.format(WARNING_DOESNT_EXIST_EXCEPTION, id);
-                    log.error(EXCEPTION_OCCURRED, new WarningExistException(errorMessage));
-                    return new WarningExistException(errorMessage);
+                    String errorMessage = String.format(WARNING_DOESNT_EXIST_EXCEPTION, warningId);
+                    log.error(EXCEPTION_OCCURRED, new WarningDoesntExistException(errorMessage));
+                    return new WarningDoesntExistException(errorMessage);
                 }));
     }
 
     @Override
     public List<WarningDTO> getAllWarnings() {
         log.info("Getting all warnings");
-        return WarningMapper.mapWarningsToWarningsDTO(warningRepository.findAll());
+        return WarningMapper.mapWarningListToWarningDTOList(warningRepository.findAll());
     }
 
     @Override
     public void deleteWarning(String username) {
-        User userWarned = findByUsername(username);
         log.info("Deleting last warning of user {}", username);
-        if (userWarned.isBlocked()) {
-            String errorMessage = String.format(USER_BANNED_EXCEPTION, username);
-            log.error(EXCEPTION_OCCURRED, new UserBlockException(errorMessage));
-            throw new UserBlockException(errorMessage);
-        }
-        if (userWarned.getWarnings().isEmpty()) {
+        User user = findUserByUsername(username);
+        validateUserBlocked(user);
+        if (user.getWarnings().isEmpty()) {
             String errorMessage = String.format(USER_IS_NOT_WARNED_EXCEPTION, username);
             log.error(EXCEPTION_OCCURRED, new UserBlockException(errorMessage));
             throw new UserBlockException(errorMessage);
         }
-        int lastWarning = userWarned.getWarnings().size() - 1;
-        Warning warning = userWarned.getWarnings().get(lastWarning);
+        int lastWarning = user.getWarnings().size() - 1;
+        Warning warning = user.getWarnings().get(lastWarning);
+        user.getWarnings().remove(warning);
         log.info("Deleted last warning of user {}", username);
         warningRepository.delete(warning);
     }
