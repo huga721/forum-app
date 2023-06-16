e9cb5ff6b526ff645654afeb79ed0c1fa65ae9ba
huga721 <szabahubert@gmail.com>
2023-06-16 09:39:34 +0200

    Updated docker container class.

--- src/test/java/huberts/spring/forumapp/ContainerIT.java
+++ src/test/java/huberts/spring/forumapp/ContainerIT.java
@@ -1,30 +1,41 @@
 package huberts.spring.forumapp;
 
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @Testcontainers
 @TestPropertySource(locations = "classpath:application-test.properties")
-public class ContainerIT {
+public class ContainerIT extends MySQLContainer<ContainerIT> implements BeforeAllCallback {
 
-    @Container
-    private static MySQLContainer<?> mySQLContainer =
-            new MySQLContainer<>("mysql:latest")
-                    .withDatabaseName("testdb")
-                    .withUsername("testuser")
-                    .withPassword("testpassword");
+    private static final String IMAGE_VERSION = "mysql:latest";
+    private static final String DATABASE_NAME = "testdb";
+    private static final String USERNAME = "testuser";
+    private static final String PASSWORD = "testpassword";
 
-    @DynamicPropertySource
-    public static void setDataSourceProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
-        registry.add("spring.datasource.username", mySQLContainer::getUsername);
-        registry.add("spring.datasource.password", mySQLContainer::getPassword);
-        registry.add("spring.datasource.driverClassName", mySQLContainer::getDriverClassName);
+    public static ContainerIT container = new ContainerIT()
+                    .withDatabaseName(DATABASE_NAME)
+                    .withUsername(USERNAME)
+                    .withPassword(PASSWORD);
+
+    public ContainerIT() {
+        super(IMAGE_VERSION);
+    }
+
+    @Override
+    public void beforeAll(ExtensionContext extensionContext) throws Exception {
+        ContainerIT.container.start();
+        setDataSourceProperties(ContainerIT.container);
+    }
+
+    private void setDataSourceProperties(ContainerIT registry) {
+        System.setProperty("spring.datasource.url", container.getJdbcUrl());
+        System.setProperty("spring.datasource.username", container.getUsername());
+        System.setProperty("spring.datasource.password", container.getPassword());
+        System.setProperty("spring.datasource.driverClassName", container.getDriverClassName());
     }
 }
